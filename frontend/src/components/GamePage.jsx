38596d867b25b1ee58efc833a363874fcef46751
BragmaDev <mh.bragge@gmail.com>
2024-02-03 01:06:34 +0200

    SKS-26 Updated game page with basic functionality

--- frontend/src/components/GamePage.jsx
+++ frontend/src/components/GamePage.jsx
@@ -1,41 +1,87 @@
-import { Box, Paper, Typography } from "@mui/material";
-
-// Class for company data
-class Company {
-    constructor(name, marketCap) {
-        this.name = name;
-        this.marketCap = marketCap;
-    }
-}
-
-let company1 = new Company("Company A", "$100,000")
-let company2 = new Company("Company B", "$200,000")
-let company3 = new Company("Company C", "$50,000")
-
-function handleClick() {
-    return
-}
-
-function ScrollPanel({ companyName, marketCap, color }) {
-    return (
-        <Box className="scroll-panel" sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", minWidth: "50vw", height: "100vh", backgroundColor: color }}>
-            <Typography variant="h2" sx={{ textAlign: "center", marginBottom: "32px" }}>
-                {companyName}
-            </Typography>
-            <Box component="img" src="https://picsum.photos/400" sx={{ width: "400px", height: "400px", borderRadius: "16px" }}></Box>
-            <Typography variant="h4" sx={{ padding: "32px 0px 6px 0px" }}>Market Cap</Typography>
-            <Typography variant="h2">{marketCap}</Typography>
-        </Box>
-    )
-}
+import { Box, ButtonBase, Paper, Typography } from "@mui/material";
+import { baseUri } from "../config.js";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function GamePage({ highScore = 0 }) {
+    const navigate = useNavigate();
+    let [score, setScore] = useState(0);
+    let [companies, setCompanies] = useState([]);
+
+    const ScrollPanel = ({ id, companyName, marketCap, color, imageSrc }) => {
+        // Function that handles game logic when a panel is clicked
+        const handleClick = () => {
+            let clickedPanel = document.getElementById(id);
+
+            if (companies[0].id === clickedPanel.id) {
+                // Left panel was clicked (corresponds to company with index 0 in 'companies')
+                if (companies[0].marketCap >= companies[1].marketCap) {
+                    setScore(score += 1);
+                    companies.splice(0, 1)
+                } else {
+                    navigate("/gameover");
+                }
+
+            } else if (companies[1].id === clickedPanel.id) {
+                // Right panel was clicked (corresponds to company with index 1 in 'companies')
+                if (companies[1].marketCap >= companies[0].marketCap) {
+                    setScore(score += 1);
+                    companies.splice(0, 1)
+                } else {
+                    navigate("/gameover");
+                }
+            }
+        };
+
+        return (
+            <ButtonBase id={id} className="scroll-panel" onClick={handleClick} sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                minWidth: "50vw",
+                height: "100vh",
+                backgroundColor: color,        
+                '&:hover': {
+                    opacity: 0.85,
+                    transition: "0.2s",
+                }
+            }}>
+                <Typography variant="h2" sx={{ textAlign: "center", marginBottom: "32px" }}>
+                    {companyName}
+                </Typography>
+                <Box component="img" src={imageSrc} sx={{ width: "400px", height: "400px", borderRadius: "16px" }}></Box>
+                <Typography variant="h4" sx={{ padding: "32px 0px 6px 0px" }}>Market Cap</Typography>
+                <Typography variant="h2">{marketCap}</Typography>
+            </ButtonBase>
+        )
+    }
+
+    // Generating companies
+    if (companies.length <= 3) {
+        for (let i = 0; i < 4; i++) {
+            if (companies.length >= 4) {
+                break;
+            }
+            // TODO: API calls for actual data
+
+            let color = "palegreen"
+            
+            if (companies.length == 0 || companies[companies.length - 1].color == "palegreen") {
+                color = "mediumaquamarine"
+            }
+
+            companies.push({ id: score.toString() + i.toString(), name: "Company Name", marketCap: Math.floor(Math.random() * 100000000), color, imageSrc: "https://picsum.photos/seed/" + Math.random() + "/400" });
+        }
+    }
+
     return (
         <div>
             <Box className="scroller" sx={{ display: "flex", flexDirection: "row", overflowX: "hidden" }}>
-                <ScrollPanel companyName={company1.name} marketCap={company1.marketCap} color={"palegreen"}></ScrollPanel>
-                <ScrollPanel companyName={company2.name} marketCap={company2.marketCap} color={"mediumaquamarine"}></ScrollPanel>
-                <ScrollPanel companyName={company3.name} marketCap={company3.marketCap} color={"palegreen"}></ScrollPanel>
+                {companies.map(company => {
+                    return <ScrollPanel key={company.id} id={company.id} companyName={company.name} marketCap={company.marketCap} color={company.color} imageSrc={company.imageSrc}></ScrollPanel>
+                })}
                 <Typography data-testid="text-highscore" sx={{ position: "absolute", top: 16, left: 16 }}>
                     High Score: {highScore}
                 </Typography>
@@ -43,7 +89,7 @@ export default function GamePage({ highScore = 0 }) {
             <Box className="score-wrapper" sx={{ display: "flex", width: "100vw", justifyContent: "center" }}>
                 <Paper elevation={4} sx={{ position: "absolute", textAlign: "center", top: -4, zIndex: 1 }}>
                     <Typography variant="h5" sx={{ color: "black", padding: "16px 32px 6px 32px" }}>SCORE</Typography>
-                    <Typography data-testid="text-score" variant="h3" sx={{ color: "black", padding: "0px 32px 12px 32px" }}>0</Typography>
+                    <Typography data-testid="text-score" variant="h3" sx={{ color: "black", padding: "0px 32px 12px 32px" }}>{score}</Typography>
                 </Paper>
             </Box>
         </div>
