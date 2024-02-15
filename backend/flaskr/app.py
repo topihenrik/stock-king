944df67f5de66541508896e07dc60168a5970325
Leevi <joona.mlin@gmail.com>
2024-02-15 19:39:37 +0200

    SKS-34 Add test for db upserts

--- backend/flaskr/app.py
+++ backend/flaskr/app.py
@@ -3,12 +3,10 @@ from flask_cors import CORS
 from flaskr import utils
 from flaskr import tickers
 import yfinance as yahoo
-from forex_python.converter import CurrencyRates
+
 import random
 import os
-import pandas as pd
-from datetime import date
-from psycopg2 import sql
+
 
 TICKERS = tickers.TICKERS
 
@@ -69,115 +67,3 @@ def loadStockDataFromDB():
     # gameData = db.query('SELECT * FROM TICKERS')
     # sendToFrontend
     return
-
-
-def get_stock_data():
-    current_date = date.today()
-    stock_data = []
-    tickers = yahoo.Tickers("GOOGL AMD AAPL")
-
-    for ticker in tickers.tickers.values():
-        try:
-            stock_data.append(
-                {
-                    "ticker": ticker.info["symbol"],
-                    "name": ticker.info["shortName"],
-                    "market_cap": ticker.info["marketCap"],
-                    "currency": ticker.info["financialCurrency"],
-                    "date": current_date,
-                    "sector": ticker.info["sector"],
-                }
-            )
-        except KeyError:
-            print("Failed to get market cap information on {}", ticker.ticker["symbol"])
-            continue
-
-    return stock_data
-
-
-def upsert_stock_data():
-    """
-    Function for upserting stock data into the "company" table in the database.
-    """
-    data = get_stock_data()
-    with utils.connect_to_db() as conn:
-        with conn.cursor() as cursor:
-            for row in data:
-                # Construct SQL query
-                query = sql.SQL(
-                    """
-                    INSERT INTO Company (ticker, name, market_cap, currency, date, sector)
-                    VALUES (%s, %s, %s, %s, %s, %s)
-                    ON CONFLICT (ticker) DO UPDATE
-                    SET market_cap = EXCLUDED.market_cap,
-                        date = EXCLUDED.date,
-                        currency = EXCLUDED.currency;
-                """
-                )
-                # Execute the query
-                cursor.execute(
-                    query,
-                    (
-                        row["ticker"],
-                        row["name"],
-                        row["market_cap"],
-                        row["currency"],
-                        row["date"],
-                        row["sector"],
-                    ),
-                )
-
-            conn.commit()
-
-
-def get_currencies_from_database():
-    """
-    Function to get all currencies from the database.
-    """
-    with utils.connect_to_db() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("SELECT DISTINCT currency FROM Company;")
-            currencies = cursor.fetchall()
-            currencies = [currency[0] for currency in currencies]
-    return currencies
-
-
-def get_exchange_rates_from_api():
-    """
-    Function for getting exchange rates from Forex API.
-    """
-    existing_currencies = get_currencies_from_database()
-    c = CurrencyRates()
-    rates = c.get_rates("USD")
-    needed_currency_rates = [
-        {currency: rates[currency]}
-        for currency in existing_currencies
-        if currency != "USD"
-    ]
-    return needed_currency_rates
-
-
-def upsert_exchange_rates():
-    """
-    Function for upserting exchange rates into the "exchange_rate" table in the database.
-    """
-    current_date = date.today()
-    data = get_exchange_rates_from_api()
-    with utils.connect_to_db() as conn:
-        with conn.cursor() as cursor:
-            for row in data:
-                currency = list(row.keys())[0]
-                # Revert the exchange rate to get the rate from the currency to USD
-                rate = 1 / row[currency]
-                # Construct SQL query
-                query = sql.SQL(
-                    """
-                    INSERT INTO ExchangeRates (from_currency, to_currency, ratio, date)
-                    VALUES (%s, %s, %s, %s)
-                    ON CONFLICT (from_currency, to_currency) DO UPDATE
-                    SET ratio = EXCLUDED.ratio;
-                """
-                )
-                # Execute the query
-                cursor.execute(query, (currency, "USD", rate, current_date))
-            conn.commit()
--- backend/tests/test_basics.py
+++ backend/tests/test_basics.py
@@ -1,6 +1,14 @@
-def test_hello_world(client):
-    assert 1 == 1
+from datetime import date
 
-def test_hello_world(client):
-   response = client.get('/api')
-   assert b"Hello World!" in response.data
+
+def test_process_currency_data(utils):
+    rates = ["USD", "EUR"]
+    existing_currencies = {
+        "EUR": 0.9372071227741331,
+        "SEK": 10.880974695407685,
+        "NOK": 11.131208997188379,
+        "PHP": 56.08434864104967,
+    }
+    expected = [{"EUR": 0.9372071227741331}]
+
+    assert utils.process_currency_data(rates, existing_currencies) == expected
