d0de42f1795b7a9456922270c3f57b7d8e11dfa7
Topi Ilmoni <topi.ilmoni@gmail.com>
2024-03-04 13:07:56 +0200

    SKS-30 Removed TestPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import TestPage from "./components/TestPage.jsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {QueryClientProvider} from "@tanstack/react-query";
 import {ThemeProvider} from "@mui/material";
-import "./style.css"
+import { theme, queryClient } from './config.js'
 import GameOverPage from "./components/GameOverPage.jsx";
 import GamePage from "./components/GamePage.jsx";
 import MenuPage from "./components/MenuPage.jsx"
-import { theme, queryClient } from './config.js'
+import "./style.css"
 
 export default function App() {
     return (
