4e3941be45bcfcf7d6bd580f1772f11ca1544692
ainoal <77739788+ainoal@users.noreply.github.com>
2024-03-13 10:19:41 +0200

    SKS-51 Consider JSON params excluded_tickers, wanted_categories and count for MSW

--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -2,6 +2,7 @@ import { expect, afterEach } from 'vitest';
 import {act, cleanup} from '@testing-library/react';
 import * as matchers from "@testing-library/jest-dom/matchers";
 import {http, HttpResponse} from "msw";
+//import { rest } from "msw";
 import {companies} from "./mock-data/companies.js";
 import {setupServer} from "msw/node";
 
@@ -10,8 +11,19 @@ vi.mock("zustand");
 expect.extend(matchers);
 
 export const restHandlers = [
-    http.post('http://localhost:5000/api/get_companies', () => {
-        return HttpResponse.json(companies)
+    http.post('http://localhost:5000/api/get_companies', async ({ request }) => {
+        const { excluded_tickers, wanted_categories, currency, count } = await request.json();
+
+        // Construct the response based on the request parameters
+        let response = companies.filter(company => !excluded_tickers.includes(company.ticker));
+        if (!(wanted_categories.length === 0)) {
+            response = response.filter(response => wanted_categories.includes(response.sector))
+        } 
+        response = response.slice(0, count);
+        
+        //console.log(response);
+
+        return HttpResponse.json(response)
     }),
 ];
 
